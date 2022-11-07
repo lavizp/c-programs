d0ef24bc8e104e641f6bc4e1d74cafc9cea3f524
Laviz-pand <pandeylaviz@gmail.com>
2022-11-07 20:51:12 +0545

    Update

--- q3.c
+++
@@ -1,9 +0,0 @@
-//3. Program to find simple interest.
-#include<stdio.h>
-void main(){
-    float p,t,r,si;
-    printf("Enter principal, time and rate respectively:");
-    scanf("%f %f %f",&p,&t,&r);
-    si=(p*t*r)/100;
-    printf("The simple interest of the given amount is %.2f",si);
-}
